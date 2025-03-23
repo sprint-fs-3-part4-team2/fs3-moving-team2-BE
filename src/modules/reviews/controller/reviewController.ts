10ee1d029a05745ed4eb13bfb0fbbd42dd451155
Yousuk <yousuk88@gmail.com>
2025-03-23 13:38:09 +0900

    feat: BE-작성가능한리뷰 유저기능 및 권한 추가

--- src/modules/reviews/controller/reviewController.ts
+++ src/modules/reviews/controller/reviewController.ts
@@ -1,27 +1,39 @@
-import { Request, Response } from 'express';
+import { RequestHandler } from 'express';
 import * as reviewService from '../service/reviewService';
-import * as completedService from '../service/completedService'; // 이 부분 수정했습니다 확인 부탁드려요
 
-export async function getPendingReviews(req: Request, res: Response) {
+export const getPendingReviews: RequestHandler = async (req, res) => {
+  const userId = req.user?.userId;
+  if (!userId) {
+    res.status(401).json({ message: '로그인 필요' });
+    return;
+  }
   try {
-    const pendingReviews = await reviewService.getPendingReviews();
+    const pendingReviews = await reviewService.getPendingReviews(userId);
     res.status(200).json(pendingReviews);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: '데이터 조회 실패' });
   }
-}
+};
 
-export async function submitReview(req: Request, res: Response) {
+export const submitReview: RequestHandler = async (req, res) => {
+  const userId = req.user?.userId;
+  if (!userId) {
+    res.status(401).json({ message: '로그인 필요' });
+    return;
+  }
   try {
     const { estimateId, rating, comment } = req.body;
-    const newReview = await reviewService.createReview({ estimateId, rating, comment });
+    const newReview = await reviewService.createReview({ userId, estimateId, rating, comment });
     res.status(201).json({
       message: '리뷰가 성공적으로 제출되었습니다.',
       data: newReview,
     });
-  } catch (error) {
+  } catch (error: unknown) {
     console.error(error);
-    res.status(500).json({ error: '리뷰 제출에 실패했습니다.' });
+    const errorMessage = error instanceof Error ? error.message : '알 수 없는 에러';
+    res.status(errorMessage === '해당 견적에 대한 리뷰 작성 권한이 없습니다.' ? 403 : 500).json({
+      error: errorMessage || '리뷰 제출에 실패했습니다.',
+    });
   }
-}
+};
