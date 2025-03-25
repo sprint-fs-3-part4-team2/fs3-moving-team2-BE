71d65c64d2631e4d3f5b4ef0cf6022dee5114fe8
heziss <heziss0965@gmail.com>
2025-03-25 18:51:19 +0900

    feat: app.ts 파일에 라우터 추가

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,7 @@ import authRouter from './modules/auth/routes';
 import notificationRouter from './modules/notification/routes';
 import rejectionRouter from './modules/rejection/routes';
 import profileRouter from './modules/profile/routes';
+import moverRoutr from './modules/movers/routes';
 
 dotenv.config();
 
@@ -57,6 +58,7 @@ app.use('/quote-requests', quoteRequestRouter);
 app.use('/notification', notificationRouter);
 app.use('/rejection', rejectionRouter);
 app.use('/profile', profileRouter);
+app.use('/movers', moverRoutr);
 
 // 서버 실행
 app.listen(port, () => {
--- src/modules/movers/routes.ts
+++ src/modules/movers/routes.ts
@@ -5,9 +5,9 @@ const router = Router();
 const moverController = new MoverController();
 
 // 기사님 목록 조회
-router.get('/', (req, res) => moverController.getMovers(req, res));
+router.get('/movers', (req, res) => moverController.getMovers(req, res));
 
 // 기사님 검색
-router.get('/search', async (req, res) => await moverController.searchMovers(req, res));
+router.get('/movers/search', async (req, res) => await moverController.searchMovers(req, res));
 
 export default router;
