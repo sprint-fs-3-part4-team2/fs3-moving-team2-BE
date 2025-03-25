b21b24ba57675944d027b146f47cffd79d3731bd
Zero5338 <a78559@naver.com>
2025-03-25 19:08:42 +0900

    fix:기사님마이페이지 콘솔삭제

--- src/modules/reviews/controller/completedController.ts
+++ src/modules/reviews/controller/completedController.ts
@@ -3,12 +3,10 @@ import getReviewsByUserId from '../service/completedService';
 
 export async function getSubmittedReviews(req: Request, res: Response) {
   const userId = req.user?.userId ?? '';
-  // const id = req.user?.userId;
-  // console.log(userId);
+
   try {
     const submittedReviews = await getReviewsByUserId(userId);
     res.status(200).json(submittedReviews);
-    // console.log(getSubmittedReviews);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: '리뷰 조회에 실패했습니다.' });
