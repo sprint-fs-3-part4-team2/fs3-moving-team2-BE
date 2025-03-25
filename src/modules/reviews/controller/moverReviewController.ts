29d8e0c35bfc581d03df6caeb3ad4b1d85f9fa21
Zero5338 <a78559@naver.com>
2025-03-25 19:02:23 +0900

    feat:기사님마이페이지 백엔드구현

---
+++ src/modules/reviews/controller/moverReviewController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { getMoverReviews } from '../service/moverReview';
+
+export async function getMoverSubmittedReviews(req: Request, res: Response) {
+  const userId = req?.user?.userId ?? '';
+  // const { id } = req.params;
+  console.log(userId);
+  try {
+    const submittedMoverReviews = await getMoverReviews(userId);
+    res.status(200).json(submittedMoverReviews);
+    // console.log(submittedMoverReviews);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: '리뷰 조회에 실패했습니다.' });
+  }
+}
---
+++ src/modules/reviews/service/moverReview.ts
@@ -0,0 +1,74 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function getMoverReviews(userId: string) {
+  const reviews = await prisma.review.findMany({
+    where: {
+      quoteMatch: {
+        moverQuote: {
+          moverId: userId,
+        },
+      },
+    },
+    select: {
+      rating: true,
+      content: true,
+      createdAt: true,
+      quoteMatch: {
+        select: {
+          moverQuote: {
+            select: {
+              mover: {
+                select: {
+                  user: {
+                    select: {
+                      id: true, // 유저 닉네임
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  // 별점 평균계산
+  const averageResult = await prisma.review.aggregate({
+    where: {
+      quoteMatch: {
+        moverQuote: {
+          moverId: userId,
+        },
+      },
+    },
+    _avg: {
+      rating: true,
+    },
+  });
+
+  const averageRating = averageResult._avg.rating ?? 0;
+
+  console.log(reviews);
+  console.log(averageRating);
+  return {
+    averageRating,
+    reviews: reviews.map((review) => ({
+      rating: review.rating,
+      content: review.content,
+      writtenAt: review.createdAt.toISOString().split('T')[0],
+      nickname: review.quoteMatch.moverQuote.mover.user.id,
+    })),
+  };
+}
+
+//별점평균
+//유저이름
+//작성일
+//별점
+//리뷰내용
