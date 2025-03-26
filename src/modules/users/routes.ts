4c2b2f092f02231960d3cc766ebd3c14b6dd0bb7
heonq <gkagjsrb@naver.com>
2025-03-26 16:06:12 +0900

    feat: 유저 관련 기능 구현

--- src/modules/users/routes.ts
+++ src/modules/users/routes.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
-import userControlelr from './controller/controller';
 import { createAuthMiddleware } from '@/core/middleware/auth/auth';
+import UserRepository from './repository/user.repository';
+import prismaClient from '@/prismaClient';
+import UserService from './service/user.service';
+import UserController from './controller/user.controller';
 
 const userRouter = Router();
 
+export const userRepository = new UserRepository(prismaClient);
+const userService = new UserService(userRepository);
+const userController = new UserController(userService);
+
+const { moverBasicInfoEdit } = userController;
+
 // 기사 - 기본 정보 수정
-userRouter.post(
-  '/mover/baiscinfo/edit',
-  createAuthMiddleware('mover'),
-  userControlelr.MoverBaiscInfoEdit,
-);
+userRouter.post('/mover/baiscinfo/edit', createAuthMiddleware('mover'), moverBasicInfoEdit);
 
 export default userRouter;
