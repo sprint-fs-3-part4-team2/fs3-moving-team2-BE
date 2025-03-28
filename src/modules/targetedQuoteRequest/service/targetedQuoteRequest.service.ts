25a91137b7a497a98f90c424f5901b8b0986d252
강대원 <alexnori.com@gmail.com>
2025-03-28 17:41:00 +0900

    remove: 필요없는 코드 제거

--- src/modules/targetedQuoteRequest/service/targetedQuoteRequest.service.ts
+++ src/modules/targetedQuoteRequest/service/targetedQuoteRequest.service.ts
@@ -28,7 +28,7 @@ export default class TargetedQuoteRequestService {
         throw new Error('견적 상태 테이블의 Quote status is not QUOTE_REQUESTED');
       }
 
-      // 3. 지정 견적 요청(TargetedQuoteRequest) 조회 (기사님 id와 견적 요청 id 기준)
+      // 2. 지정 견적 요청(TargetedQuoteRequest) 조회 (기사님 id와 견적 요청 id 기준)
       const targetedQuoteRequest = await this.targetedQuoteRequestRepository.findOneByQuoteAndMover(
         quoteId,
         moverId,
