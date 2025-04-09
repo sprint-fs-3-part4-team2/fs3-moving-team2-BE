aefe5a1f766022ce460e114e3c8e5dda3300050b
강대원 <alexnori.com@gmail.com>
2025-04-09 09:42:15 +0900

    fix: 견적 요청 만료 상태 변수명 수정

--- src/types/quoteStatus.types.ts
+++ src/types/quoteStatus.types.ts
@@ -2,13 +2,13 @@ export type QuoteStatus =
   | 'QUOTE_REQUESTED'
   | 'QUOTE_CONFIRMED'
   | 'QUOTE_CANCELED'
-  | 'REQUEST_EXPIRED'
+  | 'QUOTE_EXPIRED'
   | 'MOVE_COMPLETED';
 
 // * 견적 상태 종류
 // * QUOTE_REQUESTED: 견적 요청
 // * QUOTE_CONFIRMED: 견적 확정
 // * QUOTE_CANCELED: 견적 취소
-// * REQUEST_EXPIRED: 요청 만료
+// * QUOTE_EXPIRED: 요청 만료
 // * MOVE_COMPLETED: 이사 완료
 // *
