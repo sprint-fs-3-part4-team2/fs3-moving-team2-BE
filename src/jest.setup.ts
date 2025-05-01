f07dd1d760eb53839fb08101e26784381096e2bb
heonq <gkagjsrb@naver.com>
2025-05-01 18:25:32 +0900

    fix: test 관련 설정 수정

--- src/jest.setup.ts
+++ src/jest.setup.ts
@@ -1,15 +1,4 @@
-import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 
 // 테스트 환경에서는 .env.test 파일을 사용
 dotenv.config({ path: '.env.test' });
-
-export const prismaTestClient = new PrismaClient();
-
-beforeAll(async () => {
-  await prismaTestClient.$connect();
-});
-
-afterAll(async () => {
-  await prismaTestClient.$disconnect();
-});
