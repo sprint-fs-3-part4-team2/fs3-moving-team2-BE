895fe34b86df3779d6935c49c8f935e3e68312b3
heonq <gkagjsrb@naver.com>
2025-05-13 11:39:16 +0900

    feat: extractUserMiddleware 수정
    - 토큰 검증에 실패하면 쿠키에 있는 accessToken을 삭제하도록 설정

--- src/core/middleware/auth/extractUser.ts
+++ src/core/middleware/auth/extractUser.ts
@@ -1,3 +1,4 @@
+import { COOKIE_OPTIONS } from '@/constants/cookieOptions';
 import { NextFunction, Request, Response } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
@@ -11,6 +12,7 @@ export const extractUserMiddleware = (req: Request, res: Response, next: NextFun
     const decoded = jwt.verify(accessToken, process.env.JWT_SECRET!) as JwtPayload;
     req.userInfo = { userId: decoded.userId, type: decoded.type, roleId: decoded.roleId };
   } catch {
+    res.clearCookie('accessToken', COOKIE_OPTIONS);
     console.error('userId 조회 실패');
   }
   next();
